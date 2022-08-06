258ffc2ec64640ff967e1b274ba6a79fc83f2076
Sacha Weatherstone <sachaw100@hotmail.com>
2022-08-06 13:57:21 +1000

    Fix max length for wifi credentials

--- src/validation/config/wifi.ts
+++ src/validation/config/wifi.ts
@@ -3,10 +3,10 @@ import { Length } from "class-validator";
 import type { Protobuf } from "@meshtastic/meshtasticjs";
 
 export class WiFiValidation implements Protobuf.Config_WiFiConfig {
-  @Length(1, 30)
+  @Length(1, 33)
   ssid: string;
 
-  @Length(8, 16)
+  @Length(8, 64)
   psk: string;
 
   apMode: boolean;
