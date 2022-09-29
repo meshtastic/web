829ec6d42120177f93b3e891e231190909dcacbc
Sacha Weatherstone <sachaw100@hotmail.com>
2022-09-29 20:08:20 +1000

    Dependency and font family update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,19 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
   theme: {
+    fontFamily: {
+      mono: [
+        "Cascadia Code",
+        "ui-monospace",
+        "SFMono-Regular",
+        "Menlo",
+        "Monaco",
+        "Consolas",
+        "Liberation Mono",
+        "Courier New",
+        "monospace",
+      ],
+    },
     extend: {},
   },
   plugins: [],
