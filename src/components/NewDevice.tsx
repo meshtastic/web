079a3b6c52474894d49e595ff4e45b15a841018f
Adam McQuilkin <46639306+ajmcquilkin@users.noreply.github.com>
2022-10-04 17:56:30 -0400

    Updated unneeded imports

--- src/components/NewDevice.tsx
+++ src/components/NewDevice.tsx
@@ -1,5 +1,4 @@
-import type React from "react";
-import { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 import { FiBluetooth, FiTerminal, FiWifi } from "react-icons/fi";
 
