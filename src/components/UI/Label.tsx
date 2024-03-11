40c2ca75fc8809a88c259895b3b6ac9270e99570
David Power <david.lpower@gmail.com>
2024-03-11 21:24:50 +0100

    Revert style changes

--- src/components/UI/Label.tsx
+++ src/components/UI/Label.tsx
@@ -10,7 +10,7 @@ const Label = React.forwardRef<
   <LabelPrimitive.Root
     ref={ref}
     className={cn(
-      "text-sm font-medium leading-none node-disabled:cursor-not-allowed node-disabled:opacity-70",
+      "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
       className,
     )}
     {...props}
