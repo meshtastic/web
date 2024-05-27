569c2daa0950841a416c98d3067510a04da1c376
Tom Fifield <tom@tomfifield.net>
2024-05-27 20:57:09 +0800

    Fix traceroute ordering for multi-hop traceroutes

--- src/components/PageComponents/Messages/TraceRoute.tsx
+++ src/components/PageComponents/Messages/TraceRoute.tsx
@@ -18,17 +18,17 @@ export const TraceRoute = ({
   return route.length == 0 ? (
     <div className="ml-5 flex">
       <span className="ml-4 border-l-2 border-l-backgroundPrimary pl-2 text-textPrimary">
-      {from?.user?.longName}↔{to?.user?.longName}
+      {to?.user?.longName}↔{from?.user?.longName}
       </span>
     </div>
    ) : ( 
     <div className="ml-5 flex">
       <span className="ml-4 border-l-2 border-l-backgroundPrimary pl-2 text-textPrimary">
-      {from?.user?.longName}↔
+      {to?.user?.longName}↔
       {route.map((hop) => (
-         nodes.get(hop)?.user?.longName ?? "Unknown" + "↔"
+         nodes.get(hop)?.user?.longName + "↔" ?? "Unknown↔"
      ))}
-      {to?.user?.longName}
+      {from?.user?.longName}
       </span>
     </div>
   );
