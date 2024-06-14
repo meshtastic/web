69f7c8fd9ad2e0c74a49e024d91692c2e36d5f8a
Hunter Thornsberry <hunter@hunterthornsberry.com>
2024-06-14 18:05:59 -0400

    duplicate of #177

--- src/components/UI/Command.tsx
+++ src/components/UI/Command.tsx
@@ -116,7 +116,7 @@ const CommandItem = React.forwardRef<
   <CommandPrimitive.Item
     ref={ref}
     className={cn(
-      "relative flex cursor-default select-none items-center rounded-md py-1.5 px-2 text-sm font-medium outline-none aria-selected:bg-slate-100 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 dark:aria-selected:bg-slate-700",
+      "relative flex cursor-default select-none items-center rounded-md py-1.5 px-2 text-sm font-medium outline-none aria-selected:bg-slate-100 data-[disabled='true']:pointer-events-none data-[disabled='true']:opacity-50 dark:aria-selected:bg-slate-700",
       className,
     )}
     {...props}
