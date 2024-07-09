51215ec94f69f59476e8c64e7713fec839ce35c9
Nick Oliver <i@pixnbits.org>
2024-07-08 18:18:44 -0700

    style(test): format example Playwright tests to project's

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -5,33 +5,49 @@ test("Basic page load", async ({ page }) => {
   await expect(page).toHaveTitle("Meshtastic Web");
 });
 
-test("Prompts adding a new connection as the call to action when there are no devices", async ({page}) => {
+test("Prompts adding a new connection as the call to action when there are no devices", async ({
+  page,
+}) => {
   await page.goto("/");
 
   await expect(page).toHaveScreenshot();
 
   await expect(page.getByRole("heading", { name: "No Devices" })).toBeVisible();
-  await expect(page.getByRole("button", { name: "New Connection" })).toBeVisible();
+  await expect(
+    page.getByRole("button", { name: "New Connection" }),
+  ).toBeVisible();
 });
 
-test("Shows the protocols as the first step of adding a device", async ({page}) => {
+test("Shows the protocols as the first step of adding a device", async ({
+  page,
+}) => {
   await page.goto("/");
   await page.getByRole("button", { name: "New Connection" }).click();
-  await expect(page.getByRole("heading", { name: "Connect New Device" })).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "Connect New Device" }),
+  ).toBeVisible();
   await expect(page).toHaveScreenshot();
 });
 
-test("Requests the IP address or hostname as input when connecting over HTTP", async ({page}) => {
+test("Requests the IP address or hostname as input when connecting over HTTP", async ({
+  page,
+}) => {
   await page.goto("/");
   await page.getByRole("button", { name: "New Connection" }).click();
   await page.getByRole("tab", { name: "HTTP" }).click();
-  await expect(page.getByRole('dialog')).toHaveScreenshot('connect-over-http.png');
+  await expect(page.getByRole("dialog")).toHaveScreenshot(
+    "connect-over-http.png",
+  );
 });
 
-test("New Device button for the brower's dialog UI is presented when connecting over serial", async ({page}) => {
+test("New Device button for the brower's dialog UI is presented when connecting over serial", async ({
+  page,
+}) => {
   await page.goto("/");
   await page.getByRole("button", { name: "New Connection" }).click();
   await page.getByRole("tab", { name: "Serial" }).click();
   await expect(page.getByRole("button", { name: "New Device" })).toBeVisible();
-  await expect(page.getByRole('dialog')).toHaveScreenshot('connect-over-serial.png');
+  await expect(page.getByRole("dialog")).toHaveScreenshot(
+    "connect-over-serial.png",
+  );
 });
