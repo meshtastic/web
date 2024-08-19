4c4be2e18fffbcb63dfc8e57388a7d648a64f1cb
rcarteraz <robert.l.carter2@gmail.com>
2024-08-19 13:20:56 -0700

    temp solution

---
+++ src/components/UI/Footer.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer
+      className="flex justify-center p-4"
+      style={{
+        backgroundColor: "var(--backgroundPrimary)",
+        color: "var(--textPrimary)",
+      }}
+    >
+      <p>
+        <a
+          href="https://vercel.com/?utm_source=meshtastic&utm_campaign=oss"
+          className="hover:underline"
+          style={{ color: "var(--link)" }}
+        >
+          Powered by ▲ Vercel
+        </a>{" "}
+        | Meshtastic® is a registered trademark of Meshtastic LLC. |
+        <a
+          href="/docs/legal"
+          className="hover:underline"
+          style={{ color: "var(--link)" }}
+        >
+          {" "}
+          Legal Information
+        </a>
+        .
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
