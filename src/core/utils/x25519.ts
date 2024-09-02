d0bd02980de89146341133e34cc152eb0ca2b107
Tilen Komel <me@komelt.dev>
2024-09-02 13:10:59 +0200

    Update private key generation

--- src/core/utils/x25519.ts
+++ src/core/utils/x25519.ts
@@ -1,7 +1,13 @@
 import { x25519 } from "@noble/curves/ed25519";
 
 export function getX25519PrivateKey(): Uint8Array {
-  return x25519.utils.randomPrivateKey();
+  const key = x25519.utils.randomPrivateKey();
+
+  key[0] &= 248
+  key[31] &= 127
+  key[31] |= 64
+
+  return key;
 }
 
 export function getX25519PublicKey(privateKey: Uint8Array): Uint8Array {
