fed6b2a6da48418fbc04f924fa16b5c061194ce2
Kyle Wistrand <kyle.wistrand@gmail.com>
2024-11-26 18:45:59 -0800

    feat: Add Node detail popup in Map view

---
+++ src/components/UI/Typography/H5.tsx
@@ -0,0 +1,14 @@
+import { cn } from "@app/core/utils/cn.ts";
+
+export interface H5Props {
+  className?: string;
+  children: React.ReactNode;
+}
+
+export const H5 = ({ className, children }: H5Props): JSX.Element => (
+  <h5
+    className={cn("scroll-m-20 text-lg font-medium tracking-tight", className)}
+  >
+    {children}
+  </h5>
+);
