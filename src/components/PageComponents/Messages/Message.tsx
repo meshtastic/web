13ed974c8b846764995865946119b2d7bb03b567
PhotoNomad0 <bruce.mclean@unfoldingword.org>
2025-01-11 16:17:42 -0500

    Added date to messages

--- src/components/PageComponents/Messages/Message.tsx
+++ src/components/PageComponents/Messages/Message.tsx
@@ -45,9 +45,12 @@ export const Message = ({
           {sender?.user?.longName ?? "UNK"}
         </span>
         <span className="mt-1 font-mono text-xs text-textSecondary">
+          {message.rxTime.toLocaleDateString()} 
+        </span>
+        <span className="mt-1 font-mono text-xs text-textSecondary">
           {message.rxTime.toLocaleTimeString(undefined, {
-            hour: "2-digit",
-            minute: "2-digit",
+              hour: "2-digit",
+              minute: "2-digit",
           })}
         </span>
       </div>
