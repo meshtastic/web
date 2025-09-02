3582499a3cb3ea686bba8915e4dd073bf4333384
Jeremy Gallant <8975765+philon-@users.noreply.github.com>
2025-09-02 03:57:51 +0200

    Seperator -> Separator (#823)
    Co-authored-by: philon- <philon-@users.noreply.github.com>

--- packages/web/src/components/PageComponents/Map/NodeDetail.tsx
+++ packages/web/src/components/PageComponents/Map/NodeDetail.tsx
@@ -2,7 +2,7 @@ import BatteryStatus from "@components/BatteryStatus.tsx";
 import { Mono } from "@components/generic/Mono.tsx";
 import { TimeAgo } from "@components/generic/TimeAgo.tsx";
 import { Avatar } from "@components/UI/Avatar.tsx";
-import { Separator } from "@components/UI/Seperator.tsx";
+import { Separator } from "@components/UI/Separator";
 import { Heading } from "@components/UI/Typography/Heading.tsx";
 import { Subtle } from "@components/UI/Typography/Subtle.tsx";
 import { formatQuantity } from "@core/utils/string.ts";
